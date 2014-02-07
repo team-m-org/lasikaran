645d20f852de73ab87229b1b4be462579145d6aa
msikwal <mahesh.sikwal@morningstar.com>
2014-02-07 12:09:31 +0530

    For adding templates

--- war/module/app/js/pregnancy.js
+++ war/module/app/js/pregnancy.js
@@ -15,6 +15,27 @@ define(function(require){
 				$(".pre_details .btn-primary").on("click", function(e) {
 					return self.validateHandler.call(this,e,self);
 				});
+				$(".pre_details .js-ongoingmonth").on("change", function(e) {
+					return self.displayExpectedDate.call(this,e,self);
+				});
+			},
+			displayExpectedDate : function(e,self){
+				var ongoingMonth  = $(this).val();
+				var expectedMonth = 9 - parseInt(ongoingMonth);
+				//alert(expectedMonth);
+				if(expectedMonth > 1){
+					var today 	= new Date();
+					var dd 		= today.getDate();
+					var mm 		= today.getMonth()+expectedMonth; 
+					var yyyy 	= today.getFullYear();
+					if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} 
+					var expectedDate = mm+'/'+dd+'/'+yyyy;
+					var test = new Date(expectedDate).toGMTString();
+					$(".d_expectedMonth").html(test);
+					$(".d_expectedMonth").parent().removeClass('hide');
+				}else{
+					$(".d_expectedMonth").parent().addClass('hide');
+				}
 			},
 			validateHandler  :function(e,self){
 				var inputArr = $("#p_frm").serializeArray();
